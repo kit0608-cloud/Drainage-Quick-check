716fc29a1831fdca6f8757ca04a339e047ad6529
kit0608-cloud <kit.0608@gmail.com>
2025-11-28 23:38:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-st.title("🇬🇧 UK Drainage Quick-Check")
+st.title("UK Drainage Quick-Check")
 st.caption("For quick estimation only")
 st.markdown("---")
 
