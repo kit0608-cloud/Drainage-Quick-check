dea4902e3ca97a207fcbc3fd318a05d0f0899938
kit0608-cloud <kit.0608@gmail.com>
2025-11-30 15:11:55 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-st.title("🇬🇧 UK Drainage Quick-Check")
+st.title("🇬🇧 The Civil Engineer's Pocket Toolkit")
 st.caption("Site Estimator: Rainfall | Pipes | Ponds")
 st.markdown("---")
 
